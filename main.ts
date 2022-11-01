ac1057441cafb19f00bae4de649e3f8cf30213b8
THOMASLeumg <115450612+THOMASLeumg@users.noreply.github.com>
2022-11-01 18:46:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,39 @@
 let X_value = 0
 let Y_value = 0
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.clearScreen()
-        led.plot(X_value, 0)
-        basic.pause(500)
-        X_value += 1
-    }
-    if (X_value > 4) {
-        basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(X_value, Y_value)
+            basic.pause(500)
+            X_value += 1
+        }
         X_value = 0
-        led.plot(X_value, 0)
-    }
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        basic.clearScreen()
-        led.plot(0, Y_value)
-        basic.pause(500)
         Y_value += 1
     }
-    if (Y_value > 4) {
-        basic.clearScreen()
-        Y_value = 0
-        led.plot(0, Y_value)
+})
+input.onButtonPressed(Button.B, function () {
+    Y_value = 0
+    X_value = 0
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(X_value, Y_value)
+            basic.pause(500)
+            X_value += 1
+        }
+        Y_value += 1
+        X_value = 5
+        for (let index = 0; index < 5; index++) {
+            X_value += -1
+            led.plot(X_value, Y_value)
+            basic.pause(500)
+        }
+        Y_value += 1
+    }
+    X_value = 0
+    for (let index = 0; index < 5; index++) {
+        led.plot(X_value, Y_value)
+        basic.pause(500)
+        X_value += 1
     }
 })
